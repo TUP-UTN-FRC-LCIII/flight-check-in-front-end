09cf034da8dcc032e4fd1e723a869bca25ad140d
TUP-UTN-FRC-LCIII <tup.utn.frc.lciii@gmail.com>
2024-11-23 17:32:27 -0300

    change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   production: true,
   apis: {
     // URLs directas a los servicios de cada grupo con su docker-compose
-    be: 'https://my-json-server.typicode.com/TUP-UTN-FRC-LCIII/flight-check-in-dummy/'
+    be: 'http://flights:8081/'
   }
 };
